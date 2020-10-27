de8084c9e0df6ef22e1fa23242db748b1ea4b52e
Emily White <13emwhite13@gmail.com>
2020-10-27 17:57:05 -0500

    added heart icon

--- fitness-mvp/src/App.jsx
+++ fitness-mvp/src/App.jsx
@@ -1,15 +1,20 @@
+import './App.css';
 // import logo from './logo.svg';
-// import './App.css';
 
 import React from 'react';
-import Workouts from './Workouts.jsx'
+import Workouts from './Workouts.jsx';
+
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Emily White MVP</h1>
-        <Workouts />
+      <h1 class='title'>Emily White MVP</h1>
+      <Workouts />
       </div>
     );
   }
--- fitness-mvp/src/Workouts.jsx
+++ fitness-mvp/src/Workouts.jsx
@@ -2,49 +2,57 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 
 import Checkbox from '@material-ui/core/Checkbox';
-// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-// import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
+
 
 const Workouts = () => {
   return (
-  <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>Completed</th>
-      <th>Exercise</th>
-      <th>Description</th>
-      <th>Reps</th>
-      <th>Rounds</th>
-      <th>Weights</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    <th><Checkbox/></th>
-      <td>1</td>
-      <td>blood</td>
-      <td>10</td>
-      <td>3</td>
-      <td>8</td>
-    </tr>
-     <tr>
-     <th><Checkbox/></th>
-      <td>2</td>
-      <td>sweat</td>
-      <td>10</td>
-      <td>3</td>
-      <td>8</td>
-    </tr>
-     <tr>
-     <th><Checkbox/></th>
-      <td>3</td>
-      <td>tears</td>
-      <td>10</td>
-      <td>3</td>
-      <td>8</td>
-    </tr>
-  </tbody>
-</Table>
+    <div>
+      <FormControlLabel
+        control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" />}
+        label="ARMS"
+      />
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Completed</th>
+            <th>Exercise</th>
+            <th>Description</th>
+            <th>Reps</th>
+            <th>Rounds</th>
+            <th>Weights</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <th><Checkbox/></th>
+            <td>Bicep</td>
+            <td>Description</td>
+            <td>10</td>
+            <td>3</td>
+            <td>8</td>
+          </tr>
+          <tr>
+          <th><Checkbox/></th>
+            <td>Tricep</td>
+            <td>sweat</td>
+            <td>10</td>
+            <td>3</td>
+            <td>8</td>
+          </tr>
+          <tr>
+          <th><Checkbox/></th>
+            <td>Sholders</td>
+            <td>tears</td>
+            <td>10</td>
+            <td>3</td>
+            <td>8</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
