bd8c56109ba1bb5fa6859345a2afbfde2c993f23
Emily White <13emwhite13@gmail.com>
2020-10-27 14:42:20 -0500

    set up mvp structure

---
+++ fitness-mvp/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Emily White MVP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
